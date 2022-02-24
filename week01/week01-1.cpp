7c80cec83c62aad3fafefbac4de1734ae8848c57
周榆真 <91170624+yuchenchou@users.noreply.github.com>
2022-02-24 17:26:23 +0800

    Add files via upload

---
+++ week01/week01-1.cpp
@@ -0,0 +1,9 @@
+#include  <stdio.h>
+int main()
+{
+    int n;
+    printf("請輸入一個數值:");
+    scanf("%d",&n);
+    printf("你讀到了:%d\n",n);
+    return 0;
+}
---
+++ week01/week01-2.cpp
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+int main()
+{
+    char c;
+    printf("請輸入一堆數值:");
+    scanf("%c",&c);
+    printf("你讀到了:%c\n",c);
+    return 0;
+}
+
