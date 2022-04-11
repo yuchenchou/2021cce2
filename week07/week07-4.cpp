f1a35fab3adac0c3f9c0b357898aa8b3aa17c973
周榆真 <91170624+yuchenchou@users.noreply.github.com>
2022-04-11 19:25:10 +0800

    Add files via upload

---
+++ week07/week07-4.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char line[2001][80];
+int compare(const void * p1, const void * p2){
+	return strcmp( (char*)p1, (char*)p2 );
+}
+int main(){
+	int N;
+	scanf("%d\n", &N);
+
+	for(int i = 0; i < N; i++){
+		scanf("%s", line[i]);
+		char others[80];
+		gets(others);
+	}
+	qsort(line, N, 80, compare);
+
+	line[N][0] = 0;
+	int combo = 1;
+	for(int i = 0; i < N; i++){
+		if(strcmp(line[i], line[i+1]) == 0){
+		combo++;
+		}
+		else{
+		printf("%s %d\n", line[i], combo);
+		combo=1;
+			}
+	}
+	return 0;
+}
